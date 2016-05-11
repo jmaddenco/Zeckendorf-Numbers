c60936414cb106572f99c1ae7c4de97b7f9c5700
Josh Madden <jmaddenco@gmail.com>
2016-05-11 10:51:52 -0600

    refine work

--- src/Zeckendorf.java
+++ src/Zeckendorf.java
@@ -1,35 +1,47 @@
-import java.util.*;
- 
-class Zeckendorf
-{
-  public static String getZeckendorf(int n)
-  {
-    if (n == 0)
-      return "0";
-    List<Integer> fibNumbers = new ArrayList<Integer>();
-    fibNumbers.add(1);
-    int nextFib = 2;
-    while (nextFib <= n)
-    {
-      fibNumbers.add(nextFib);
-      nextFib += fibNumbers.get(fibNumbers.size() - 2);
+import java.util.ArrayList;
+
+public class Zeckendorf {
+
+  private static final int[] fibs = {13, 8, 5, 3, 2, 1};
+
+  public static void main(String[] args) {
+    for (int i = 0; i <= 15; i++) {
+      if (i == 0) {
+        System.out.print("0");
+
+      } else {
+        System.out.println(gen(i));
+      }
     }
-    StringBuilder sb = new StringBuilder();
-    for (int i = fibNumbers.size() - 1; i >= 0; i--)
-    {
-      int fibNumber = fibNumbers.get(i);
-      sb.append((fibNumber <= n) ? "1" : "0");
-      if (fibNumber <= n)
-        n -= fibNumber;
-    }
-    return sb.toString();
   }
- 
-  public static void main(String[] args)
-  {
-    for (int i = 0; i <= 20; i++)
-      System.out.println("Zeckendorf(" + i + ")=" + getZeckendorf(i));
+
+  public static String gen(int num) {
+    ArrayList<Integer> zeros = new ArrayList<>();
+    int sum = 0;
+    for (int fib : fibs) {
+      if (sum + fib >= num) {
+        zeros.add(0);
+      } else {
+        zeros.add(1);
+        sum = sum + fib;
+      }
+    }
+    return makeString(zeros);
+  }
+
+  public static String makeString(ArrayList<Integer> zeros) {
+    String n = "";
+    boolean firstOne = false;
+    for (int i: zeros) {
+      if (i != 0) {
+        n = n + i;
+        firstOne = true;
+      } else {
+        if (firstOne) {
+          n = n + i;
+        }
+      }
+    }
+    return n;
   }
 }
-
-
